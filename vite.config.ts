0eca8993fa45ef360c401ee13e784302d4002a57
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2024-02-28 22:36:52 -0600

    Update base url to work once deployed to bucket

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.BASE_URL || "/",
   plugins: [vue(), vuetify()],
   resolve: {
     alias: {
