6e7392f9de4d298e54bb034f3e234d5272dd6629
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2024-03-05 20:41:41 -0600

    Updated DrawingStats to be editable

--- src/components/MapDrawing/DrawingStats.vue
+++ src/components/MapDrawing/DrawingStats.vue
@@ -1,18 +1,54 @@
 <script setup lang="ts">
 import { useDataPackageStore } from "@/stores/dataPackage";
+import GetComment from "../GetComment.vue";
+import { ref } from "vue";
+import type { DrawnArea } from "@/types";
 
 const dataPackageStore = useDataPackageStore();
+
+const commentGetter = ref<InstanceType<typeof GetComment>>();
+
+const updateData = async (area: DrawnArea) => {
+  const updateIndex = dataPackageStore.dataPackage.drawnAreas.findIndex((a) => a.id === area.id);
+
+  if (updateIndex === -1) {
+    return;
+  }
+
+  const { comments, fencedInYard, accessibleFromStreet, stairsToAccess, location } =
+    await commentGetter.value!.getComment({
+      comments: area.comments,
+      fencedInYard: area.fencedInYard ?? false,
+      accessibleFromStreet: area.accessibleFromStreet ?? false,
+      stairsToAccess: area.stairsToAccess ?? false,
+      location: area.location
+    });
+
+  dataPackageStore.dataPackage.drawnAreas[updateIndex] = {
+    ...area,
+    comments,
+    fencedInYard,
+    accessibleFromStreet,
+    stairsToAccess,
+    location
+  };
+};
 </script>
 
 <template>
   <v-scale-transition>
     <v-card style="max-width: 300px">
+      <GetComment ref="commentGetter" />
       <v-card-title>Drawn Stats</v-card-title>
 
       <v-expansion-panels>
         <v-expansion-panel v-for="area in dataPackageStore.dataPackage.drawnAreas" :key="area.area">
           <v-expansion-panel-title>
-            {{ area.id }}: {{ area.type ?? "Sod" }} - {{ Math.round(area.area).toLocaleString() }} SQFT
+            <span>{{ area.id }}: {{ area.type ?? "Sod" }} - {{ Math.round(area.area).toLocaleString() }} SQFT</span>
+            <v-spacer />
+            <v-btn icon variant="text" size="small" @click.stop="updateData(area)">
+              <v-icon icon="mdi-pencil" />
+            </v-btn>
           </v-expansion-panel-title>
           <v-expansion-panel-text>
             <v-container>
