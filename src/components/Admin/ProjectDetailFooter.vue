12a2073f17609b96ab9f5ed69d5692c0a7d43639
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2024-05-24 22:30:25 -0500

    Added some tools to better manage cost breakdown of projects

---
+++ src/components/Admin/ProjectDetailFooter.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import { useDataPackageStore } from "@/stores/dataPackage";
+import { useCostTools } from "@/composables/useCostTools";
+
+const dataPackageStore = useDataPackageStore();
+const { getProjectTier, getProjectCost } = useCostTools();
+
+const typeRatio = ref(0);
+
+const totalArea = computed(() => dataPackageStore.dataPackage.drawnAreas.reduce((acc, cur) => acc + cur.area, 0) ?? 0);
+const kbgArea = computed(() => Math.round(totalArea.value * (typeRatio.value / 100)));
+const stArea = computed(() => Math.round(totalArea.value * ((100 - typeRatio.value) / 100)));
+const costBreakdown = computed(() => getProjectCost(kbgArea.value, stArea.value) ?? {});
+</script>
+
+<template>
+  <div class="d-flex justify-center mb-4">
+    <v-chip class="mx-2"> {{ getProjectTier(totalArea)?.name ?? "Unknown" }} </v-chip>
+    <v-chip class="mx-2">
+      KBG: {{ kbgArea }} ft²
+      <span v-if="costBreakdown.kbgCost > 0" class="ms-1">(${{ Math.round(costBreakdown.kbgCost) }})</span>
+    </v-chip>
+    <v-chip class="mx-2">
+      ST: {{ stArea }} ft²
+      <span v-if="costBreakdown.stCost > 0" class="ms-1">(${{ Math.round(costBreakdown.stCost) }})</span>
+    </v-chip>
+    <v-chip v-if="costBreakdown.totalCost > 0" class="mx-2">
+      Total Cost: ${{ Math.round(costBreakdown.totalCost) }}
+    </v-chip>
+  </div>
+  <div class="font-weight-bold">Grass Type Ratio</div>
+  <div class="d-flex justify-center">
+    <v-slider
+      v-model="typeRatio"
+      thumb-label="always"
+      thumb-size="24"
+      label="Grass Type Ratio"
+      track-color="secondary"
+      class="mx-2"
+    >
+      <template #label>
+        <v-badge color="primary" class="ms-1 me-3" />
+        {{ Math.round(100 - typeRatio) }}% Kentucky Blue Grass
+        <v-badge color="secondary" class="ms-6 me-3" />
+        {{ Math.round(typeRatio) }}% St. Augustine
+      </template>
+      <template #thumb-label>
+        <div>{{ Math.round(typeRatio) }}%</div>
+      </template>
+    </v-slider>
+  </div>
+</template>
--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import ProjectDetailFooter from "@/components/Admin/ProjectDetailFooter.vue";
 import MapWidgetRoot from "@/components/MapDrawing/MapWidgetRoot.vue";
 </script>
 
 <template>
   <v-main>
     <v-container flat class="px-lg-10 px-4">
-      <MapWidgetRoot />
+      <MapWidgetRoot class="mb-2" />
+      <ProjectDetailFooter />
     </v-container>
   </v-main>
 </template>
